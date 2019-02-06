548308e791257dd35339c9f9481bc992935f686f
Caroline Cange <carolinecang@gmail.com>
2019-02-06 12:07:11 -0800

    basic setup

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, './client/src');
+var DIST_DIR = path.join(__dirname, './client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.jsx`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        loader: "babel-loader",
+        test: /\.js[x]?/, //js or jsx file for use
+        exclude: /node_modules/,
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"]
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+
+};
