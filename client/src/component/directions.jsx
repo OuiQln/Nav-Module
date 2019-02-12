4ee07f3c89a8bdde18ba693c7c5a945eecde7f5b
Caroline Cange <carolinecang@gmail.com>
2019-02-11 23:29:35 -0800

    basic drop-down

--- client/src/component/directions.jsx
+++ client/src/component/directions.jsx
@@ -12,26 +12,209 @@ const Directions=()=> {
       </div>
 
       <div className={sl.tags}>
-        <div>
+
+        <div className={sl.dropdown}>
           <a href="" className={sl.a}><strong className={sl.choices}>WOMEN</strong></a>
+            <div className={sl.womenselection}>
+              <a href=""><strong>SHOP BY CATEGORY</strong></a>
+              <a href="">Outerwear and Blazers</a>
+              <a href="">Ultra Light Down Collection</a>
+              <a href="">Sweatshirts and Sweatpants</a>
+              <a href="">Sweaters and Cardigans</a>
+              <a href="">T-Shirts and Tops</a>
+              <a href="">UT: Graphic Tees</a>
+              <a href="">Dresses and Jumpsuits</a>
+              <a href="">Skirts</a>
+              <a href="">Jeans</a>
+              <a href="">Pants</a>
+              <a href="">Leggings Pants</a>
+              <a href="">NEW Shorts</a>
+              <a href="">Activewear</a>
+              <a href="">Innerwear</a>
+              <a href="">NEW Bra Tops</a>
+              <a href="">Bras and Underwear</a>
+              <a href="">Socks and Hosiery</a>
+              <a href="">Loungewear</a>
+              <a href="">Accessories and Shoes</a>
+              <a href="">NEW ARRIVALS</a>
+              <a href="">LIMITED-TIME OFFERS</a>
+              <a href="">UNIQLO ESSENTIALS</a>
+              <a href="">EXTENDED SIZES</a>
+              <a href="">BULK ORDERS</a>
+              <a href="">SALE</a>
+              <a href="">BLOG</a>
+              <a href="">FEATURED COLLECTIONS</a>
+              <a href="">NEW Linen Collection</a>
+              <a href="">The 365 Shop</a>
+              <a href="">Maternity</a>
+              <a href="">HEATTECH Collection</a>
+              <a href="">AIRism Collection</a>
+              <a href="">SPECIAL COLLABORATIONS</a>
+              <a href="">NEW SPRZ NY</a>
+              <a href="">NEW UNIQLO U</a>
+              <a href="">NEW Princess Way</a>
+              <a href="">View All ></a>
+            </div>
         </div>
-        <div>
-          <a href="" className={sl.a}><strong className={sl.choices}>MEN</strong></a> 
+
+
+        <div className={sl.dropdown}>
+          <a href="" className={sl.a}><strong className={sl.choices}>MEN</strong></a>
+          <div className={sl.menselection}>
+            <a href=""><strong>SHOP BY CATEGORY</strong></a>
+            <a href="">Outerwear and Blazers</a>
+            <a href="">Ultra Light Down Collection</a>
+            <a href="">Sweatshirts and Sweatpants</a>
+            <a href="">Sweaters and Cardigans</a>
+            <a href="">T-Shirts and Tops</a>
+            <a href="">UT: Graphic Tees</a>
+            <a href="">Dresses and Jumpsuits</a>
+            <a href="">Skirts</a>
+            <a href="">Jeans</a>
+            <a href="">Pants</a>
+            <a href="">Leggings Pants</a>
+            <a href="">NEW Shorts</a>
+            <a href="">Activewear</a>
+            <a href="">Innerwear</a>
+            <a href="">NEW Bra Tops</a>
+            <a href="">Bras and Underwear</a>
+            <a href="">Socks and Hosiery</a>
+            <a href="">Loungewear</a>
+            <a href="">Accessories and Shoes</a>
+            <a href="">NEW ARRIVALS</a>
+            <a href="">LIMITED-TIME OFFERS</a>
+            <a href="">UNIQLO ESSENTIALS</a>
+            <a href="">EXTENDED SIZES</a>
+            <a href="">BULK ORDERS</a>
+            <a href="">SALE</a>
+            <a href="">BLOG</a>
+            <a href="">FEATURED COLLECTIONS</a>
+            <a href="">NEW Linen Collection</a>
+            <a href="">The 365 Shop</a>
+            <a href="">Maternity</a>
+            <a href="">HEATTECH Collection</a>
+            <a href="">AIRism Collection</a>
+            <a href="">SPECIAL COLLABORATIONS</a>
+            <a href="">NEW SPRZ NY</a>
+            <a href="">NEW UNIQLO U</a>
+            <a href="">NEW Princess Way</a>
+            <a href="">View All ></a>
+          </div>
         </div>
-        <div>
+        <div className={sl.dropdown}>
           <a href="" className={sl.a}><strong className={sl.choices}>GIRLS</strong></a>
+          <div className={sl.girlselection}>
+            <a href=""><strong>SHOP BY CATEGORY</strong></a>
+            <a href="">Outerwear</a>
+            <a href="">Ultra Light Down Collection</a>
+            <a href="">Sweatshirts and Sweatpants</a>
+            <a href="">T-Shirts and Tops</a>
+            <a href="">UT: Graphic Tees</a>
+            <a href="">Pants and Shorts</a>
+            <a href="">Dress and Skirts</a>
+            <a href="">Activewear</a>
+            <a href="">Innerwear and Loungewear</a>
+            <a href="">HEATTECH Collection</a>
+            <a href="">AIRism Collection</a>
+            <a href="">Accessories</a>
+            <a href="">NEW ARRIVALS</a>
+            <a href="">SALE</a>
+            <a href="">FEATURED COLLECTIONS</a>
+            <a href="">The 365 Shop</a>
+            <a href="">Online Exclusives</a>
+            <a href="">SPECIAL COLLABORATIONS</a>
+            <a href="">NEW LEGO</a>
+            <a href="">Blossoming Dreams</a>
+            <a href="">Magic For All</a>
+            <a href="">Peanuts</a>
+            <a href="">Pieter Ceizer</a>
+            <a href="">Kaws x Desame Street</a>
+            <a href="">View All ></a>
+          </div>
         </div>
-        <div>
+
+        <div className={sl.dropdown}>
           <a href="" className={sl.a}><strong className={sl.choices}>BOYS</strong></a>
+          <div className={sl.boyselection}>
+            <a href=""><strong>SHOP BY CATEGORY</strong></a>
+            <a href="">Outerwear</a>
+            <a href="">Ultra Light Down Collection</a>
+            <a href="">Sweatshirts and Sweatpants</a>
+            <a href="">T-Shirts and Tops</a>
+            <a href="">UT: Graphic Tees</a>
+            <a href="">Pants and Shorts</a>
+            <a href="">Dress and Skirts</a>
+            <a href="">Activewear</a>
+            <a href="">Innerwear and Loungewear</a>
+            <a href="">HEATTECH Collection</a>
+            <a href="">AIRism Collection</a>
+            <a href="">Accessories</a>
+            <a href="">NEW ARRIVALS</a>
+            <a href="">SALE</a>
+            <a href="">FEATURED COLLECTIONS</a>
+            <a href="">The 365 Shop</a>
+            <a href="">Online Exclusives</a>
+            <a href="">SPECIAL COLLABORATIONS</a>
+            <a href="">NEW LEGO</a>
+            <a href="">Blossoming Dreams</a>
+            <a href="">Magic For All</a>
+            <a href="">Peanuts</a>
+            <a href="">Pieter Ceizer</a>
+            <a href="">Kaws x Desame Street</a>
+            <a href="">View All ></a>
+          </div>
         </div>
-        <div>
+
+        <div className={sl.dropdown}>
           <a href="" className={sl.a}><strong className={sl.choices}>BABY</strong></a>
+          <div className={sl.babyselection}>
+            <a href=""><strong>SHOP BY CATEGORY</strong></a>
+            <a href="">Outerwear</a>
+            <a href="">Ultra Light Down Collection</a>
+            <a href="">Sweatshirts and Sweatpants</a>
+            <a href="">T-Shirts and Tops</a>
+            <a href="">UT: Graphic Tees</a>
+            <a href="">Pants and Shorts</a>
+            <a href="">Dress and Skirts</a>
+            <a href="">Activewear</a>
+            <a href="">Innerwear and Loungewear</a>
+            <a href="">HEATTECH Collection</a>
+            <a href="">AIRism Collection</a>
+            <a href="">Accessories</a>
+            <a href="">NEW ARRIVALS</a>
+            <a href="">SALE</a>
+            <a href="">FEATURED COLLECTIONS</a>
+            <a href="">The 365 Shop</a>
+            <a href="">Online Exclusives</a>
+            <a href="">SPECIAL COLLABORATIONS</a>
+            <a href="">NEW LEGO</a>
+            <a href="">Blossoming Dreams</a>
+            <a href="">Magic For All</a>
+            <a href="">Peanuts</a>
+            <a href="">Pieter Ceizer</a>
+            <a href="">Kaws x Desame Street</a>
+            <a href="">View All ></a>
+          </div>
         </div>
-        <div>
+
+        <div className={sl.dropdown}>
           <a href="" className={sl.a}><strong className={sl.choices}>NEW</strong></a>
+          <div className={sl.newselection}>
+            <a href=""><strong>SHOP BY CATEGORY</strong></a>
+            <a href="">Magic For All</a>
+            <a href="">Peanuts</a>
+            <a href="">Pieter Ceizer</a>
+            <a href="">Kaws x Desame Street</a>
+            <a href="">View All ></a>
+          </div>
         </div>
-        <div className={sl.sale}>
-          <a href="" className={sl.a}><strong >SALE</strong></a>
+
+        <div className={sl.dropdown}>
+          <a href="" className={sl.a}><strong className={sl.choices}>SALE</strong></a>
+          <div className={sl.saleselection}>
+            <a href="">SALE</a>
+            <a href="">View All ></a>
+          </div>
         </div>
 
       </div>
