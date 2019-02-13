9fb840e3ed141df3859609b0d18628d4c21653e4
Caroline Cange <carolinecang@gmail.com>
2019-02-13 00:20:23 -0800

    just format minor changes

--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -12,16 +12,8 @@ class Search extends React.Component {
   render(){
     return(
       <div className={sl.searchbar}>
-        {/* <div className={sl.searchtext}>
-            <strong>search</strong>
-        </div>
-        <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo"  width="25" height="25" /></div>
-        <div>
-          
-        </div> */}
-
         <input class={sl.searchBox} type="search" name="search" placeholder="Search" />
-        <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo" width="25" height="25" /></div>
+          <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo" width="25" height="25" /></div>
 
         {/* <input type="submit" value="Search" class="searchButton" /> */}
       </div>
