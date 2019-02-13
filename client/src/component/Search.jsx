c47cf6a63541d470c0b19be13da222e434f091dd
Caroline Cange <carolinecang@gmail.com>
2019-02-13 00:18:17 -0800

    added a input functional search bar

--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -5,15 +5,25 @@ class Search extends React.Component {
   constructor() {
     super();
     this.state= {
-
+      query: ''
     } 
   }
 
   render(){
     return(
       <div className={sl.searchbar}>
-        <div className={sl.searchtext}><strong>search</strong></div>
+        {/* <div className={sl.searchtext}>
+            <strong>search</strong>
+        </div>
         <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo"  width="25" height="25" /></div>
+        <div>
+          
+        </div> */}
+
+        <input class={sl.searchBox} type="search" name="search" placeholder="Search" />
+        <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo" width="25" height="25" /></div>
+
+        {/* <input type="submit" value="Search" class="searchButton" /> */}
       </div>
     );
   }
