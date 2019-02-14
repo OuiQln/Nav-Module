27b4d03003aa5f72b26a30d356b90df21a45542e
Caroline Cange <carolinecang@gmail.com>
2019-02-14 00:52:53 -0800

    added more data

--- schema.sql
+++ schema.sql
@@ -19,3 +19,25 @@ insert into product (product_type, product_price, product_image, product_descrip
 insert into product (product_type, product_price, product_image, product_description) values ('pants','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/pants6.jpeg','WOMEN EZY ANKLE-LENGTH PANTS'); 
 insert into product (product_type, product_price, product_image, product_description) values ('pants','$39.90','https://s3-us-west-1.amazonaws.com/uniqloassets/pants7.jpeg','WOMEN EZY ANKLE-LENGTH PANTS'); 
 insert into product (product_type, product_price, product_image, product_description) values ('pants','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/pants8.jpeg','WOMEN EZY ANKLE-LENGTH PANTS'); 
+
+
+
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt1.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt2.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt3.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt4.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt5.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt6.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt7.jpeg','shirt'); 
+insert into product (product_type, product_price, product_image, product_description) values ('shirts','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/shirt8.jpeg','shirt'); 
+
+
+
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater1.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater2.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater3.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater4.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater5.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater6.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater7.jpeg','sweater'); 
+insert into product (product_type, product_price, product_image, product_description) values ('sweater','$29.90','https://s3-us-west-1.amazonaws.com/uniqloassets/sweater8.jpeg','sweater'); 
