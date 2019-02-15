0415b2a1e8173ab11fe7749f5b2dcb62c552cfdb
Caroline Cange <carolinecang@gmail.com>
2019-02-15 00:21:08 -0800

    adjust css to set all in place

--- client/src/component/Search.jsx
+++ client/src/component/Search.jsx
@@ -74,36 +74,46 @@ class Search extends React.Component {
             <div className={sl.searchmarker}><img src="https://s3-us-west-1.amazonaws.com/uniqloassets/questionmark.png" alt="logo" width="25" height="25" /></div>
         </div>
 
-        <div className={sl.predict}>
-          {
-            this.state.predictions.map((item, index) => (
-              <div key={index + item}> DO YOU MEAN ? <br /> <br /> {item} <br /> <br /> 
-                CATEGORIES <br /> <br />
-                MEN > UNIQLO U > {item} <br /> 
-                BOYS > {item} AND SHORTS > {item} <br /> 
-                BABY > {item} AND LEGGINGS > {item} <br /> 
-                MEN > FLEECE > BLOCKTECH FLEECE > <br /> 
-                {item}<br /> 
-                MEN > {item} <br /> 
-                WOMEN > {item} <br /> 
-                GIRLS > ONLINE EXCLISIVES > {item} <br /> 
-                MEN > THE 365 SHOP > {item} 
-              </div>
-            ))
-          }
-        </div> 
- 
-        {listArr.length>0 && 
-          <div className={sl.dropdowns}>
-            {listArr.map(item => {
-              return <div className={sl.itemlist}>
-                <img src={item.product_image} alt='img' width="120px" height="120px"/> 
-                <p> {item.product_description}</p>
-                {item.product_price}
-              </div>
-            })}
+        {this.state.query.length>2 && this.state.list!==0 &&
+          <div className={sl.results}>
+
+            <div className={sl.predict}>
+              {
+                this.state.predictions.map((item, index) => (
+                  <div key={index + item}> DO YOU MEAN ? <br /> <br /> {item} <br /> <br /> 
+                    CATEGORIES <br /> <br />
+                    MEN > UNIQLO U > {item} <br /> 
+                    BOYS > {item} AND SHORTS > {item} <br /> 
+                    BABY > {item} AND LEGGINGS > {item} <br /> 
+                    MEN > FLEECE > BLOCKTECH FLEECE > <br /> 
+                    {item}<br /> 
+                    MEN > {item} <br /> 
+                    WOMEN > {item} <br /> 
+                    GIRLS > ONLINE EXCLISIVES > {item} <br /> 
+                    MEN > THE 365 SHOP > {item} 
+                  </div>
+                ))
+              }
+            </div> 
+    
+            {listArr.length!==0 ?
+              <div className={sl.dropdowns}>
+                <div className={sl.search_pro}>
+                  TOP RESULTS FOR {this.state.query}
+                  <button>VIEW ALL</button>
+                </div>
+
+                  {listArr.map(item => {
+                    return <div className={sl.itemlist}>
+                      <img src={item.product_image} alt='img' width="120px" height="120px"/> 
+                      <p> {item.product_description}</p>
+                      {item.product_price}
+                    </div>
+                  })}
+              </div> : <div className={sl.notshow}></div>
+            }  
           </div>
-        }  
+        }
       </div>
     );
   }
