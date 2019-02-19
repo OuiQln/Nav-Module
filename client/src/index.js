8c62f86da42a6052b041b304f65ec902ced64cf1
Caroline Cange <carolinecang@gmail.com>
2019-02-19 00:22:07 -0800

    eslint the react component

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TopBar from './component/topbar.jsx';
-import Directions from './component/directions.jsx';
-// import Direction from './component/direction.jsx';
+import TopBar from './component/topbar';
+import Directions from './component/directions';
 
 ReactDOM.render(
   <div>
     <TopBar />
     <Directions />
   </div>,
-  document.getElementById("app")
+  document.getElementById('app'),
 
 );
